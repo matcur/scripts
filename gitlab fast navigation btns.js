39e4e6005bd6e351c9ba21e73170192f966f26f9
Rustam <34961288+matcur@users.noreply.github.com>
2025-10-20 17:32:13 +0500

    Update gitlab fast navigation btns.js

--- gitlab fast navigation btns.js
+++ gitlab fast navigation btns.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Gitlab Buttons
 // @namespace    http://tampermonkey.net/
 // @version      2025-09-03
 // @description  try to take over the world!
